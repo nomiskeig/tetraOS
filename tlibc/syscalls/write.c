25364a1adc3644259ddecdc337d7bcfba133427d
Nomiskeig <nomiskeig@gmail.com>
2024-09-24 12:54:00 +0200

    tlibc: Fix write system call using the wrong system call number

--- tlibc/syscalls/write.c
+++ tlibc/syscalls/write.c
@@ -5,10 +5,7 @@ ssize_t write(int fd, const void* buf, size_t count) {
     asm("add a1, zero, %0" : : "r"(fd) :);
     asm("add a2, zero, %0" : : "r"(buf) :);
     asm("add a3, zero, %0" : : "r"(count) :);
-    //load_argument(1, fd);
-    //load_argument(2, (uint64_t)buf);
-    //load_argument(3, count);
-    make_syscall(2);
+    make_syscall(1);
     return 0;
 
 }
